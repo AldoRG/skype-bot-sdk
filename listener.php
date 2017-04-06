c19272b267febcb09c8a9fa57befcb333a683877
Aldo Ruiz <aldo.ruiz@outlook.com>
2017-04-05 23:35:15 -0600

    erturn using instance of datastorage

--- listener.php
+++ listener.php
@@ -1,12 +1,11 @@
 <?php
 	require 'vendor/autoload.php';
-	$dataStorate = "/";
+	$dataStorate = new \SkypeBot\Storage\FileStorage("/folder");
 	$config = new \SkypeBot\Config(
 		'd60336c4-982e-4a11-94ec-b14eed0c9059',
 		'BLreEhKHGhQNbva5ZHhOS1D'
 	);
 	$conversation_id = null;
-	var_dump($dataStorate);
 	$bot = \SkypeBot\SkypeBot::init($config, $dataStorate);
 	$bot->getNotificationListener()->setMessageHandler(function($payload) use (&$conversation_id) {
 			$conversation_id = $payload->getConversation()->getId();
@@ -18,12 +17,11 @@
 			var_dump("\n");
 			var_dump($payload->getConversation()->getId());
 			file_put_contents(
-				'conversation_id.txt',
+				'folder/conversation_id.txt',
 				$payload->getConversation()->getId()
 			);
 		}
 	);
-	var_dump($conversation_id);
 	$bot->getApiClient()->call(
 		new \SkypeBot\Command\SendMessage(
 			'Hello World.',
