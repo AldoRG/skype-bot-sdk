082d45efe8d56d9696da9715350408b3360e1e19
Aldo Ruiz <aldo.ruiz@outlook.com>
2017-04-05 19:21:55 -0600

    remove spaces

--- listener.php
+++ listener.php
@@ -2,22 +2,22 @@
 	require 'vendor/autoload.php';
 	$dataStorate = new \SkypeBot\Storage\FileStorage(sys_get_temp_dir());
 	$config = new \SkypeBot\Config(
-	    'd60336c4-982e-4a11-94ec-b14eed0c9059',
-	    'BLreEhKHGhQNbva5ZHhOS1D'
+		'd60336c4-982e-4a11-94ec-b14eed0c9059',
+		'BLreEhKHGhQNbva5ZHhOS1D'
 	);
 
 	$bot = \SkypeBot\SkypeBot::init($config, $dataStorate);
 	$bot->getNotificationListener()->setMessageHandler(function($payload) {
-	        file_put_contents(
-	            sys_get_temp_dir() . '/conversation_id.txt',
-	            $payload->getConversation()->getId()
-	        );
-	    }
+			file_put_contents(
+				sys_get_temp_dir() . '/conversation_id.txt',
+				$payload->getConversation()->getId()
+			);
+		}
 	);
 	$bot->getApiClient()->call(
-    new \SkypeBot\Command\SendMessage(
-        'Hello World.',
-        'Your conversation id'
-    )
-);
+		new \SkypeBot\Command\SendMessage(
+			'Hello World.',
+        	'Your conversation id'
+		)
+	);
 ?>
