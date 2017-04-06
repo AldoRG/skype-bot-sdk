23a495f8ca7175e35d4552bda023934ed2506fce
Aldo Ruiz <aldo.ruiz@outlook.com>
2017-04-06 15:56:12 -0600

    remove initialize bot

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-	file_put_contents('folder/conversation_id.txt',"asdf");
-	echo sys_get_temp_dir();	
+	require 'vendor/autoload.php';
+	$dataStorate = new \SkypeBot\Storage\FileStorage("folder");
+	$config = new \SkypeBot\Config(
+		'd60336c4-982e-4a11-94ec-b14eed0c9059',
+		'BLreEhKHGhQNbva5ZHhOS1D'
+	);	
 ?>
