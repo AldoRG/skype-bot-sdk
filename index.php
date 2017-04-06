74738d86d1da58e9976b15be6e736d5da51c152a
Aldo Ruiz <aldo.ruiz@outlook.com>
2017-04-06 16:03:20 -0600

    set original code

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
 	$config = new \SkypeBot\Config(
 		'd60336c4-982e-4a11-94ec-b14eed0c9059',
 		'BLreEhKHGhQNbva5ZHhOS1D'
-	);	
+	);
 ?>
--- listener.php
+++ listener.php
@@ -1,11 +1,21 @@
 <?php
 	require 'vendor/autoload.php';
-	
+	$dataStorate = new \SkypeBot\Storage\FileStorage("folder");
+	$config = new \SkypeBot\Config(
+		'd60336c4-982e-4a11-94ec-b14eed0c9059',
+		'BLreEhKHGhQNbva5ZHhOS1D'
+	);
 	$conversation_id = null;
 	$bot = \SkypeBot\SkypeBot::init($config, $dataStorate);
 	$bot->getNotificationListener()->setMessageHandler(function($payload){
 			$conversation_id = $payload->getConversation()->getId();
 			file_put_contents('folder/conversation_id.txt',$payload->getConversation()->getId());
+			$bot->getApiClient()->call(
+				new \SkypeBot\Command\SendMessage(
+					'Hello World.',
+					$conversation_id
+				)
+			);
 		}
 	);
 	$bot->getApiClient()->call(
