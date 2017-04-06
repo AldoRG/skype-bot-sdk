4ad0f3cd61d3c2d7c7ba82050b773148cf153bac
Aldo Ruiz <aldo.ruiz@outlook.com>
2017-04-05 20:15:40 -0600

    remove temporary

--- listener.php
+++ listener.php
@@ -1,6 +1,6 @@
 <?php
 	require 'vendor/autoload.php';
-	$dataStorate = new \SkypeBot\Storage\FileStorage(sys_get_temp_dir());
+	$dataStorate = new \SkypeBot\Storage\FileStorage("/");
 	$config = new \SkypeBot\Config(
 		'd60336c4-982e-4a11-94ec-b14eed0c9059',
 		'BLreEhKHGhQNbva5ZHhOS1D'
