fba53c524482cfdde9f53538c04c85b4b913adf0
AldoRG <aldo.ruiz@outlook.com>
2018-10-06 10:34:40 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	require 'vendor/autoload.php';
 	$dataStorate = new \SkypeBot\Storage\FileStorage("folder");
 	$config = new \SkypeBot\Config(
-		'd60336c4-982e-4a11-94ec-b14eed0c9059',
-		'BLreEhKHGhQNbva5ZHhOS1D'
+		'',
+		''
 	);
 ?>
